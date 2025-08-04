514398ac3d2cbc8c1ac41efe896993ba5bb822fd
Manjit Yadav <manjityadav9986@gmail.com>
2025-08-04 16:03:07 +0530

    second  commit

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const dbConnect = async ()=>{
-const Connect = await mongoose.connect('mongodb://localhost:27017/quirex');
+const Connect = await mongoose.connect('mongodb+srv://manjityadav9986:Manjit9986@cluster0.c4tm2uh.mongodb.net/quirex');
 if(Connect){
 console.log("database connected...");
 }
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(fileUpload());
-app.use(cors());
+app.use(cors({
+    origin : "http://localhost:5173"
+}));
 
 
 dbConnect();
