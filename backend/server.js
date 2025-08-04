2dc86467b4c7c4bff18cfed1dbba433fe0b6a173
Manjit Yadav <manjityadav9986@gmail.com>
2025-08-04 16:49:20 +0530

    adding frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(fileUpload());
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://dream-house-deal.netlify.app",
 }));
 
 
